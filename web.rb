d4aa94e10c1c86ddff6aff4020e01ba6a0b4e708
eladg <ggrelad@gmail.com>
2015-02-03 06:36:55 +0200

    set post instead of get

--- web.rb
+++ web.rb
@@ -8,7 +8,7 @@ require 'hipchat'
 Dotenv.load
 require './application.rb'
 
-get '/updated/:ticket' do
+post '/updated/:ticket' do
   message = "(chef) #{fun_hi} #{fun_everyone}! I've noticed ticket: #{params[:ticket]} was updated by #{params[:user_id]}... #{fun_emoji}\nCheck it out: https://stringwire.atlassian.net/browse/#{params[:ticket]}"
   client.send(sender, message, msg_params)
 end
