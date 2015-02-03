0305bd8344bb5b6ffe42614d7f0d771a4bc8ff4b
eladg <ggrelad@gmail.com>
2015-02-03 06:55:27 +0200

    text, color and additional routes

--- application.rb
+++ application.rb
@@ -5,7 +5,6 @@ module HNConfig
   HIPCHAT_ROOM_ID       = ENV["HIPCHAT_ROOM_ID"]
   HIPCHAT_ROOM_NAME     = ENV["HIPCHAT_ROOM_NAME"]
   HIPCHAT_MSG_FROM      = ENV["HIPCHAT_MSG_FROM"]
-  HIPCHAT_MSG_COLOR     = ENV["HIPCHAT_MSG_COLOR"]
 
   # constants
   HIPCHAT_API_ROOMS_MSG = "rooms/message"
@@ -16,10 +15,24 @@ def client
   HipChat::Client.new(HNConfig::HIPCHAT_TOKEN, :api_version => 'v2')[HNConfig::HIPCHAT_ROOM_NAME]
 end
 
-def msg_params
+def update_msg_params
   {
     :message_format => 'text',
-    :color => HNConfig::HIPCHAT_MSG_COLOR
+    :color => 'gray'
+  }
+end
+
+def create_msg_params
+  {
+    :message_format => 'text',
+    :color => 'red'
+  }
+end
+
+def closed_msg_params
+  {
+    :message_format => 'text',
+    :color => 'green'
   }
 end
 
@@ -28,7 +41,7 @@ def sender
 end
 
 def fun_hi
-  ["hey", "Hey pal!", "hi", "hi pal!", "Hello", "sup", "Hey", "Hey", "Hate", "herro", "hiiiii", "hola", "Howdy", "Ello", "hiya", "ANYTHING", "haiiii", "Haaaaayye", "Hii", "heyooo", "hi", "yo", "yo", "hey", "hihihi", "hey sexy ;)", "aloha", "bonjour", "hey", "I", "hey", "whuddup", "whaddup", "wasssuuuppp", "hullo", "heya", "HEYYYYYYY", "hayyyy", "hihasdgiii"].sample
+  ["hey", "Heeeeey", "hi", "hihihi", "Hello", "sup", "Hey", "herro", "hiiiii", "hola", "Howdy", "Ello", "hiya", "ANYTHING", "haiiii", "Haaaaayye", "Hii", "heyooo", "hi", "yo", "yo", "hey", "hihihi", "hey sexy ;)", "aloha", "bonjour", "hey", "I", "hey", "whuddup", "whaddup", "wasssuuuppp", "hullo", "heya", "HEYYYYYYY", "hayyyy", "hihasdgiii"].sample
 end
 
 def fun_everyone
@@ -39,3 +52,6 @@ def fun_emoji
   ["(allthethings)","(android)","(areyoukiddingme)","(arrington)","(arya)","(ashton)","(atlassian)","(awesome)","(awthanks)","(aww)","(awwyiss)","(awyeah)","(badass)","(badjokeeel)","(badpokerface)","(badtime)","(basket)","(beer)","(bicepleft)","(bicepright)","(bitbucket)","(boom)","(borat)","(branch)","(bumble)","(bunny)","(cadbury)","(cake)","(candycorn)","(carl)","(caruso)","(catchemall)","(ceilingcat)","(celeryman)","(cereal)","(cerealspit)","(challengeaccepted)","(chef)","(chewie)","(chocobunny)","(chompy)","(chucknorris)","(clarence)","(coffee)","(confluence)","(content)","(continue)","(cookie)","(cornelius)","(corpsethumb)","(daenerys)","(dance)","(dealwithit)","(derp)","(disappear)","(disapproval)","(doge)","(doh)","(donotwant)","(dosequis)","(downvote)","(drevil)","(drool)","(ducreux)","(dumb)","(evilburns)","(excellent)","(facepalm)","(failed)","(feelsbadman)","(feelsgoodman)","(finn)","(fireworks)","(firstworldproblems)","(fonzie)","(foreveralone)","(freddie)","(fry)","(ftfy)","(fu)","(fuckyeah)","(fwp)","(gangnamstyle)","(gates)","(ghost)","(giggity)","(goldstar)","(goodnews)","(greenbeer)","(grumpycat)","(gtfo)","(haha)","(haveaseat)","(heart)","(heygirl)","(hipchat)","(hipster)","(hodor)","(huehue)","(hugefan)","(huh)","(ilied)","(indeed)","(iseewhatyoudidthere)","(itsatrap)","(jackie)","(jaime)","(jake)","(jira)","(jobs)","(joffrey)","(jonsnow)","(kennypowers)","(krang)","(kwanzaa)","(lincoln)","(lol)","(lolwut)","(megusta)","(meh)","(menorah)","(mindblown)","(motherofgod)","(ned)","(nextgendev)","(nice)","(ninja)","(noidea)","(notbad)","(nothingtodohere)","(notit)","(notsureif)","(notsureifgusta)","(obama)","(ohcrap)","(ohgodwhy)","(ohmy)","(okay)","(omg)","(orly)","(paddlin)","(pbr)","(philosoraptor)","(pingpong)","(pirate)","(pokerface)","(poo)","(present)","(pumpkin)","(rageguy)","(rainicorn)","(rebeccablack)","(reddit)","(rockon)","(romney)","(rudolph)","(salute)","(samuel)","(santa)","(sap)","(scumbag)","(seomoz)","(shamrock)","(shrug)","(skyrim)","(standup)","(stare)","(success)","(sweetjesus)","(taco)","(taft)","(tea)","(thatthing)","(theyregreat)","(toodamnhigh)","(tree)","(troll)","(truestory)","(trump)","(turkey)","(twss)","(tyrion)","(tywin)","(unacceptable)","(washington)","(wat)","(whoa)","(whynotboth)","(wtf)","(yeah)","(yey)","(yodawg)","(youdontsay)","(yougotitdude)","(yuno)","(zoidberg)"].sample
 end
 
+def worried_prase
+  ["ohhhh", "hmm hmm hmm", "shooosh", "damn!", "oh-ho", "baaaaahh"].sample
+end
--- web.rb
+++ web.rb
@@ -9,6 +9,16 @@ Dotenv.load
 require './application.rb'
 
 post '/updated/:ticket' do
-  message = "(chef) #{fun_hi} #{fun_everyone}! I've noticed ticket: #{params[:ticket]} was updated by #{params[:user_id]}... #{fun_emoji}\nCheck it out: https://stringwire.atlassian.net/browse/#{params[:ticket]}"
-  client.send(sender, message, msg_params)
+  message = "(chef) > I've noticed ticket: #{params[:ticket]} was updated by #{params[:user_id]}... #{fun_emoji}\nCheck it out: https://stringwire.atlassian.net/browse/#{params[:ticket]}"
+  client.send(sender, message, update_msg_params)
+end
+
+post '/created/:ticket' do
+  message = "(chef) > #{worried_prase}... #{params[:user_id]} created new ticket (areyoukiddingme)\nCheck it out: https://stringwire.atlassian.net/browse/#{params[:ticket]}"
+  client.send(sender, message, create_msg_params)
+end
+
+post '/closed/:ticket' do
+  message = "(chef) > #{params[:user_id]} has closed ticket #{params[:ticket]} #{fun_emoji}#{fun_emoji}#{fun_emoji}!! yay!\nCheck it out: https://stringwire.atlassian.net/browse/#{params[:ticket]}"
+  client.send(sender, message, closed_msg_params)
 end
